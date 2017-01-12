44a6b948f6365071837ee59adc21742c6d5520be
abmoon <avraham.luna@gmail.com>
2017-01-11 19:28:19 -0800

    parsed data from the request body

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require("express");
 var app = express();
 var rooms = require("./data/rooms.json");
+var bodyParser = require("body-parser");
+var uuid = require("node-uuid");
+
 
 app.set("views", "./views");
 app.set('view engine', 'jade');
 
+
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
+app.use(bodyParser.urlencoded({ extended:true}));
+
 
 app.get('/', function (req, res) {
     res.render("index", { title:"Home"});
@@ -21,8 +27,18 @@ app.get('/admin/rooms', function (req, res) {
 
 app.get('/admin/rooms/add', function (req, res) {
     res.render("add");
+});
+
+app.post('/admin/rooms/add', function (req, res) {
+    var room = {
+        name: req.body.name,
+        id: uuid.v4()
+    };
+    rooms.push(room);
+    res.json(room);
 
 });
+
 
 app.listen(3000, function () {
     console.log('Chat app listening on port 3000!');
