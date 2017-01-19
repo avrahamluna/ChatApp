ebb645f86d72685a5dd95444b68183d57b222630
abmoon <avraham.luna@gmail.com>
2017-01-18 22:02:45 -0800

    configuration and settings of CRUD functionality

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var rooms = require("./data/rooms.json");
 var bodyParser = require("body-parser");
 var uuid = require("node-uuid");
-
+var _ = require("lodash");
 
 app.set("views", "./views");
 app.set('view engine', 'jade');
@@ -35,10 +35,47 @@ app.post('/admin/rooms/add', function (req, res) {
         id: uuid.v4()
     };
     rooms.push(room);
-    res.json(room);
+    res.redirect("/admin/rooms");
+    
 
 });
 
+
+app.get('/admin/rooms/edit/:id', function (req, res) {
+    var roomId = req.params.id;
+
+    var room = _.find(rooms, r => r.id === roomId);
+    if(!room){
+        res.sendStatus(404);
+        return;
+    }
+
+
+    res.render("edit", { room });
+
+});
+
+app.post('/admin/rooms/edit/:id', function (req, res) {
+    var roomId = req.params.id;
+
+    var room = _.find(rooms, r => r.id === roomId);
+
+    room.name = req.body.name;
+
+    res.redirect("/admin/rooms");
+
+
+});
+
+app.get('/admin/rooms/delete/:id', function (req, res) {
+    var roomId = req.params.id;
+
+    rooms = rooms.filter(r => r.id !== roomId);
+
+
+    res.redirect("/admin/rooms");
+
+});
 
 app.listen(3000, function () {
     console.log('Chat app listening on port 3000!');
